dbd02f66eb8b029d7761494ead1b9c5bea35e985
lcoandrade <lcoandrade@gmail.com>
2014-09-30 15:05:37 -0300

    Update ui_azimuthsAndDistances.py
    Adding the possibility to generate automaticaly the memorial files required by the brazilian laws.

--- azimuthsAndDistances/ui_azimuthsAndDistances.py
+++ azimuthsAndDistances/ui_azimuthsAndDistances.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'ui_azimuthsAndDistances.ui'
 #
-# Created: Sat Sep 27 17:22:23 2014
-#      by: PyQt4 UI code generator 4.10.2
+# Created: Tue Sep 30 08:28:10 2014
+#      by: PyQt4 UI code generator 4.8.3
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -12,16 +12,7 @@ from PyQt4 import QtCore, QtGui
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
 except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+    _fromUtf8 = lambda s: s
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
@@ -48,25 +39,29 @@ class Ui_Dialog(object):
         self.textEdit = QtGui.QTextEdit(Dialog)
         self.textEdit.setObjectName(_fromUtf8("textEdit"))
         self.gridLayout.addWidget(self.textEdit, 2, 0, 1, 1)
-        self.horizontalLayout_6 = QtGui.QHBoxLayout()
-        self.horizontalLayout_6.setObjectName(_fromUtf8("horizontalLayout_6"))
+        self.horizontalLayout_2 = QtGui.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(_fromUtf8("horizontalLayout_2"))
         self.calculateButton = QtGui.QPushButton(Dialog)
         self.calculateButton.setObjectName(_fromUtf8("calculateButton"))
-        self.horizontalLayout_6.addWidget(self.calculateButton)
+        self.horizontalLayout_2.addWidget(self.calculateButton)
         self.clearButton = QtGui.QPushButton(Dialog)
         self.clearButton.setObjectName(_fromUtf8("clearButton"))
-        self.horizontalLayout_6.addWidget(self.clearButton)
-        self.gridLayout.addLayout(self.horizontalLayout_6, 3, 0, 1, 1)
+        self.horizontalLayout_2.addWidget(self.clearButton)
+        self.gridLayout.addLayout(self.horizontalLayout_2, 3, 0, 1, 1)
+        self.saveFilesButton = QtGui.QPushButton(Dialog)
+        self.saveFilesButton.setObjectName(_fromUtf8("saveFilesButton"))
+        self.gridLayout.addWidget(self.saveFilesButton, 4, 0, 1, 1)
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(_translate("Dialog", "Azimuths and Distances", None))
-        self.label.setText(_translate("Dialog", "Geometry Description", None))
-        self.label_2.setText(_translate("Dialog", "Inform the Meridian Convergence:", None))
-        self.lineEdit.setToolTip(_translate("Dialog", "Use decimal degrees", None))
-        self.textEdit.setToolTip(_translate("Dialog", "CSV text with Point, E, N, side, Planar azimuth, Real Azimuth and Distance", None))
-        self.calculateButton.setText(_translate("Dialog", "Calculate", None))
-        self.clearButton.setText(_translate("Dialog", "Clear", None))
+        Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "Azimuths and Distances", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("Dialog", "Geometry Description", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_2.setText(QtGui.QApplication.translate("Dialog", "Inform the Meridian Convergence:", None, QtGui.QApplication.UnicodeUTF8))
+        self.lineEdit.setToolTip(QtGui.QApplication.translate("Dialog", "Use decimal degrees", None, QtGui.QApplication.UnicodeUTF8))
+        self.textEdit.setToolTip(QtGui.QApplication.translate("Dialog", "CSV text with Point, E, N, side, Planar azimuth, Real Azimuth and Distance", None, QtGui.QApplication.UnicodeUTF8))
+        self.calculateButton.setText(QtGui.QApplication.translate("Dialog", "Calculate", None, QtGui.QApplication.UnicodeUTF8))
+        self.clearButton.setText(QtGui.QApplication.translate("Dialog", "Clear", None, QtGui.QApplication.UnicodeUTF8))
+        self.saveFilesButton.setText(QtGui.QApplication.translate("Dialog", "Save Files", None, QtGui.QApplication.UnicodeUTF8))
 
