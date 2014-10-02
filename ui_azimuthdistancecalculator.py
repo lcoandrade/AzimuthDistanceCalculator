aa0c7b54afd3665541cd53517cb5b68d5b1903db
lcoandrade <lcoandrade@gmail.com>
2014-10-02 10:57:09 -0300

    Information about the documents generated

--- ui_azimuthdistancecalculator.py
+++ ui_azimuthdistancecalculator.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'ui_azimuthdistancecalculator.ui'
 #
-# Created: Fri Sep 26 23:38:03 2014
-#      by: PyQt4 UI code generator 4.10.2
+# Created: Thu Oct 02 10:59:43 2014
+#      by: PyQt4 UI code generator 4.8.3
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -12,34 +12,29 @@ from PyQt4 import QtCore, QtGui
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
 except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+    _fromUtf8 = lambda s: s
 
 class Ui_AzimuthDistanceCalculator(object):
     def setupUi(self, AzimuthDistanceCalculator):
         AzimuthDistanceCalculator.setObjectName(_fromUtf8("AzimuthDistanceCalculator"))
-        AzimuthDistanceCalculator.resize(362, 105)
+        AzimuthDistanceCalculator.resize(416, 170)
         self.gridLayout = QtGui.QGridLayout(AzimuthDistanceCalculator)
         self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
         self.kappaAndConvergenceButton = QtGui.QPushButton(AzimuthDistanceCalculator)
         self.kappaAndConvergenceButton.setObjectName(_fromUtf8("kappaAndConvergenceButton"))
-        self.gridLayout.addWidget(self.kappaAndConvergenceButton, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.kappaAndConvergenceButton, 0, 0, 1, 2)
         self.azimuthsAndDistancesButton = QtGui.QPushButton(AzimuthDistanceCalculator)
         self.azimuthsAndDistancesButton.setObjectName(_fromUtf8("azimuthsAndDistancesButton"))
-        self.gridLayout.addWidget(self.azimuthsAndDistancesButton, 1, 0, 1, 1)
+        self.gridLayout.addWidget(self.azimuthsAndDistancesButton, 1, 0, 1, 2)
+        self.textEdit = QtGui.QTextEdit(AzimuthDistanceCalculator)
+        self.textEdit.setEnabled(False)
+        self.textEdit.setObjectName(_fromUtf8("textEdit"))
+        self.gridLayout.addWidget(self.textEdit, 2, 0, 1, 1)
         self.buttonBox = QtGui.QDialogButtonBox(AzimuthDistanceCalculator)
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
         self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
         self.buttonBox.setObjectName(_fromUtf8("buttonBox"))
-        self.gridLayout.addWidget(self.buttonBox, 2, 0, 1, 1)
+        self.gridLayout.addWidget(self.buttonBox, 2, 1, 1, 1)
 
         self.retranslateUi(AzimuthDistanceCalculator)
         QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("accepted()")), AzimuthDistanceCalculator.accept)
@@ -47,8 +42,13 @@ class Ui_AzimuthDistanceCalculator(object):
         QtCore.QMetaObject.connectSlotsByName(AzimuthDistanceCalculator)
 
     def retranslateUi(self, AzimuthDistanceCalculator):
-        AzimuthDistanceCalculator.setWindowTitle(_translate("AzimuthDistanceCalculator", "AzimuthDistanceCalculator", None))
-        self.kappaAndConvergenceButton.setText(_translate("AzimuthDistanceCalculator", "Calculate Kappa And Convergence", None))
-        self.azimuthsAndDistancesButton.setToolTip(_translate("AzimuthDistanceCalculator", "Generates a CSV with azimuths and distances for the selected feature", None))
-        self.azimuthsAndDistancesButton.setText(_translate("AzimuthDistanceCalculator", "Calculate Azimuths and Distances", None))
+        AzimuthDistanceCalculator.setWindowTitle(QtGui.QApplication.translate("AzimuthDistanceCalculator", "AzimuthDistanceCalculator", None, QtGui.QApplication.UnicodeUTF8))
+        self.kappaAndConvergenceButton.setText(QtGui.QApplication.translate("AzimuthDistanceCalculator", "Calculate Kappa And Convergence", None, QtGui.QApplication.UnicodeUTF8))
+        self.azimuthsAndDistancesButton.setToolTip(QtGui.QApplication.translate("AzimuthDistanceCalculator", "Generates a CSV with azimuths and distances for the selected feature", None, QtGui.QApplication.UnicodeUTF8))
+        self.azimuthsAndDistancesButton.setText(QtGui.QApplication.translate("AzimuthDistanceCalculator", "Calculate Azimuths and Distances", None, QtGui.QApplication.UnicodeUTF8))
+        self.textEdit.setHtml(QtGui.QApplication.translate("AzimuthDistanceCalculator", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"justify\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:8pt; font-weight:600; color:#ff0000;\">All files generated are made according to the brazilian laws. Feel free to check the code and make the documents in your language.</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
 
